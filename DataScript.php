b807e0da57a1dd85d6c285e46ea41a5bbdbf8d23
henrywalters <henrywalters20@gmail.com>
2017-02-24 08:03:26 -0500

    Cross-browser compatibility
    IE does not support JavaScript modern class notation so I have refactored the code to match the older syntax.

--- DataScript.php
+++ DataScript.php
@@ -1,8 +1,6 @@
 <script>
 
-class Data {
-	//Note that raw_headers is an optional paramter
-	constructor(raw_data,raw_headers) {
+	function Data(raw_data,raw_headers) {
 		if (raw_data) {
 			this._raw_data = raw_data;
 			this._data = raw_data;
@@ -21,7 +19,6 @@ class Data {
 		}
 		this._filters = null;
 		this._header_filters = null;
-
 		if (!this.valid_data()){
 			console.log("ERROR: entered data was invalid and has been nullified");
 			this._raw_data = null;
@@ -29,23 +26,31 @@ class Data {
 		}
 	}
 
-	get headers() {
+	Data.prototype.headers = function(){
 		return this._headers;
 	}
 
-	get raw_headers() {
+	Data.prototype.raw_headers = function(){
 		return this._raw_headers;
 	}
 
-	get data() {
+	Data.prototype.data = function(){
 		return this._data;
 	}
 
-	get raw_data() {
+	Data.prototype.raw_data = function(){
 		return this._raw_data;
 	}
-	
-	valid_data(){
+
+	Data.prototype.update_raw_headers = function(new_headers){
+		this._raw_headers = new_headers;
+	}
+
+	Data.prototype.update_raw_data = function(raw_data){
+		this._raw_data = raw_data;
+	}
+
+	Data.prototype.valid_data = function(){
 		if (this._raw_headers){
 			var headers = this._raw_headers.length;
 		}
@@ -71,10 +76,9 @@ class Data {
 		return good_data;
 	}
 
-	raw_table(id,border,style) {
+	Data.prototype.raw_table = function(id,border,style){
 		if (this.valid_data()) {
 			var html = '<table';
-
 			if (id){
 				html += ' id="' + id + '"';
 			}
@@ -86,14 +90,14 @@ class Data {
 			}
 			html += '>';
 			if (this._raw_headers && this._raw_headers.length > 0){
-				html += '<tr>';
+				html += '<thead><tr>';
 				for (var i = 0; i < this._raw_headers.length; i++){
 					html += '<th>' + this._raw_headers[i] + '</th>';
 				} 
-				html += '</tr>';
+				html += '</tr></thead>';
 			}
-
 			if (this._raw_data && this._raw_data.length > 0){
+				html += '<tbody>';
 				for (var i = 0; i < this._raw_data.length; i++){
 					html += '<tr>';
 					for (var j = 0; j < this._raw_data[i].length; j++){
@@ -101,17 +105,16 @@ class Data {
 					}
 					html + '</tr>';
 				}
+				html += '</tbody>';
 			}
-
 			html += '</table>';
 			return html;
 		}
 	}
 
-	table(id,border,style) {
+	Data.prototype.table = function(id,border,style){
 		if (this.valid_data()) {
 			var html = '<table';
-
 			if (id){
 				html += ' id="' + id + '"';
 			}
@@ -122,16 +125,15 @@ class Data {
 				html += ' style="' + style + '"';
 			}
 			html += '>';
-
 			if (this._headers && this._headers.length > 0){
-				html += '<tr>';
+				html += '<thead><tr>';
 				for (var i = 0; i < this._headers.length; i++){
 					html += '<th>' + this._headers[i] + '</th>';
 				} 
-				html += '</tr>';
+				html += '</tr></thead>';
 			}
-
 			if (this._data && this._data.length > 0){
+				html += '<tbody>';
 				for (var i = 0; i < this._data.length; i++){
 					html += '<tr>';
 					for (var j = 0; j < this._data[i].length; j++){
@@ -139,29 +141,26 @@ class Data {
 					}
 					html + '</tr>';
 				}
+				html += '</tbody>';
 			}
-
 			html += '</table>';
 			return html;
 		}
 	}
-	//Ex: {COLUMN1, COLUMN2} => only column 1 & column 2 data will be shown.
-	set_header_filters(filters,composite,strict){
+
+	Data.prototype.set_header_filters = function(filters,composite, strict){
 		this._header_filters = {filters: filters};
 		this._header_filters['composite'] = ((composite) ? composite : false);
 		this._header_filters['strict'] = ((strict) ? strict : false);
 	}
 
-	//Ex: {COLUMN1: {'VALUE1','VALUE2'}} = {0:{'VALUE1','VALUE2'}} =>  returns all rows where 
-	//column 1 is equal to value 1 or value 2.
-	set_filters(filters){
+	Data.prototype.set_filters = function(filters){
 		this._filters = filters;
 	}
 
-	get_raw_filter_options(){
+	Data.prototype.get_raw_filter_options = function(){
 		var headers = this._raw_headers;
 		var data = this._raw_data;
-
 		var filters = {};
 		if (headers){
 			for (var row in data){
@@ -169,7 +168,7 @@ class Data {
 					if (!filters.hasOwnProperty(headers[i])){
 						filters[headers[i]] = [];
 					}
-					if (filters[headers[i]].indexOf(data[row][i]) == -1){
+					if (filters[headers[i]].indexOf(data[row][i]) == -1 && data[row][i] != ''){
 						filters[headers[i]].push(data[row][i]);
 					}
 				}
@@ -183,10 +182,10 @@ class Data {
 			console.log('WARNING: headers required for this feature');
 		}
 	}
-	get_filter_options(){
+
+	Data.prototype.get_filter_options = function(){
 		var headers = this._headers;
 		var data = this._data;
-
 		var filters = {};
 		if (headers){
 			for (var row in data){
@@ -194,7 +193,7 @@ class Data {
 					if (!filters.hasOwnProperty(headers[i])){
 						filters[headers[i]] = [];
 					}
-					if (filters[headers[i]].indexOf(data[row][i]) == -1){
+					if (filters[headers[i]].indexOf(data[row][i]) == -1  && data[row][i] != ''){
 						filters[headers[i]].push(data[row][i]);
 					}
 				}
@@ -202,51 +201,26 @@ class Data {
 			for (var filter in filters){
 				filters[filter] = filters[filter].sort();
 			}
-			console.log(filters);
 			return filters;
 		} 
 		else {
 			console.log('WARNING: headers required for this feature');
 		}
 	}
-	
-	
-	//Filter Algorithm:
-	//1.) First sieve the data through the header filters getting rid of unneccesary data. 
-	//2.) Adjust filters to match the data resulting from the first sieve.
-	//3.) Iterate through every row, and check if each cell with a filter index is 
-	//    an element of the union of the filters for that index. 
 
-	//Parameter definitions
-	// composite is a boolean (assumed false), which returns modifies step 3 of the algorithm.
-	// Let S = {{1,2,3},{2,3,4},{3,4,5}} and F = {0:{1,2}}
-	// the unique elements of S are S_u = {1,2,3,4,5}.
-	// The intersection of S_u and F is {1,2} where the composite intersection of S_u 
-	// and F is {3,4,5}. If composite is true if any cell of index 1 is an element of 
-	// {3,4,5} it will not be sieved where as if composite is false, only elements in
-	// {1,2} will be not be sieved. 
-	// In simpler terms if composite is true, if cell[n][filter_index] is an element
-	// of the union of F[filter_index] it will sieve that row.
-
-	filter_data(){
-
+	Data.prototype.filter_data = function(){
 		var headers = ((this._raw_headers) ? this._raw_headers : null);
 		var data = ((this._raw_data) ? this._raw_data : null);
-
 		var header_filters = ((this._header_filters) ? this._header_filters : {'filters':[],'composite':true});
 		var index_header = [];
-
 		for (var i = 0; i<headers.length; i++){ index_header.push(i);}
-
-		var filters = ((this._filters) ? this._filters : null);
+			var filters = ((this._filters) ? this._filters : null);
 		var tmp_header = [];
 		var tmp_header_names = [];
-
 		if (headers){
 			for (var i = 0; i < headers.length; i++){
 				if (typeof headers[i] === 'string' || headers[i] instanceof String){
 					var index = ((header_filters['filters'].indexOf(headers[i]) != -1) ? header_filters['filters'].indexOf(headers[i]) : null);
-				
 					if (index != null){
 						if (!header_filters['composite']){
 							tmp_header.push(i);
@@ -302,7 +276,6 @@ class Data {
 				}
 			}
 		}
-		
 		var tmp_data = [];
 		for (var i = 0; i < data.length; i++){
 			var data_match = true;
@@ -315,7 +288,6 @@ class Data {
 				var strict = filters[data_columns[j]]['strict'];
 				var condition = filters[data_columns[j]]['condition'];
 				var variables = filters[data_columns[j]]['variables'];
-
 				if (col_filters){
 					if (!strict)
 					{
@@ -328,7 +300,6 @@ class Data {
 							cell = cell.toUpperCase();
 						}
 					}
-
 					var valid_cell = false;
 					if (col_filters.indexOf(cell) != -1){
 						valid_cell = true;
@@ -338,57 +309,57 @@ class Data {
 					var valid_cell = false;
 					switch(condition){
 						case 'range-inclusive':
-							if (variables.length == 2 && variables[0] <= variables[1]){
-								if (cell <= variables[1] && cell >= variables[0]){
-									valid_cell = true;
-								}
-							}   
-							else {
-								console.log('range-inclusive needs an array of two sorted numbers');
+						if (variables.length == 2 && variables[0] <= variables[1]){
+							if (cell <= variables[1] && cell >= variables[0]){
+								valid_cell = true;
 							}
-							break;
+						}   
+						else {
+							console.log('range-inclusive needs an array of two sorted numbers');
+						}
+						break;
 						case 'range-exclusive':
-							if (variables.length == 2 && variables[0] < variables[1]){
-								if (cell < variables[1] && cell > variables[0]){
-									valid_cell = true;
-								}
-							}   
-							else {
-								console.log('range-inclusive needs an array of two sorted numbers');
+						if (variables.length == 2 && variables[0] < variables[1]){
+							if (cell < variables[1] && cell > variables[0]){
+								valid_cell = true;
 							}
-							break;
+						}   
+						else {
+							console.log('range-inclusive needs an array of two sorted numbers');
+						}
+						break;
 						case 'greater-than-inclusive':
-							if (cell >= variables){
-								valid_cell = true;
-							}
-							break;
+						if (cell >= variables){
+							valid_cell = true;
+						}
+						break;
 						case 'greater-than-exclusive':
-							if (cell > variables){
-								valid_cell = true;
-							}
-							break;
+						if (cell > variables){
+							valid_cell = true;
+						}
+						break;
 						case 'less-than-inclusive':
-							if (cell <= variables){
-								valid_cell = true;
-							}
-							break;
+						if (cell <= variables){
+							valid_cell = true;
+						}
+						break;
 						case 'less-than-exclusive':
-							if (cell < variables){
-								valid_cell = true;
-							}
-							break;
+						if (cell < variables){
+							valid_cell = true;
+						}
+						break;
 						case 'equals':
-							if (cell == variables){
-								valid_cell = true;
-							}
-							break;
+						if (cell == variables){
+							valid_cell = true;
+						}
+						break;
 						case 'raw-condition':
-							if (eval(variables)){
-								valid_cell = true;
-							}
-							break;
+						if (eval(variables)){
+							valid_cell = true;
+						}
+						break;
 						default:
-							break;
+						break;
 					}
 				}
 				if (valid_cell){
@@ -403,8 +374,6 @@ class Data {
 					}
 				}
 			}
-
-
 			for (var j = 0; j < indexes.length; j++){
 				tmp_row.push(data[i][indexes[j]]);
 			}
@@ -412,8 +381,7 @@ class Data {
 				tmp_data.push(tmp_row);
 			}
 		}
+
 		this._data = tmp_data;
 	}
-}
-
 </script>
